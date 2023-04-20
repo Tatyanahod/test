c0fb335b4c7f9cf4cea1c8c123512d8a8e7fd817
Tatyana Hodakovskaya <115928261+Tatyanahod@users.noreply.github.com>
2023-04-20 20:50:23 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const makeMarkup = array => {
+  return array
+    .map(
+      element =>
+        `<a class="gallery__item" href="${element.original}">
+         <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+    </a>`
+    )
+    .join('');
+};
+
+refs.gallery.insertAdjacentHTML('beforeend', makeMarkup(galleryItems));
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
